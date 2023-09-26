e5bc420ea23b9b73d0e4626ad763817b86d1060c
Emmanuel Jazon <131515197+jzonx8@users.noreply.github.com>
2023-09-26 13:16:42 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,112 @@ void drawScene();
 void initRendering();
 void handleResize(int w, int h);
 void bgc();
+void keyboardFunc(unsigned char key, int x, int y);
 
-int main(int argc, char** argv) {
+
+int currentScene = 1;
+
+void lineSteve(){
+	glBegin(GL_LINES); // head
+	glColor3f(0.87f,0.65f,0.29f);
+	glVertex2f(-4.0f,8.0f);
+	glVertex2f(4.0f,8.0f);
+	glVertex2f(4.0f,8.0f);
+	glVertex2f(4.0f,16.0f);
+	glVertex2f(4.0f,16.0f);
+	glVertex2f(-4.0f,16.0f);
+	glVertex2f(-4.0f,16.0f);
+	glVertex2f(-4.0f,8.0f);
+	glEnd();
 	
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1920, 1080);
-	glutCreateWindow("Steve");
+	glBegin(GL_POLYGON); // Torso
+	glColor3f(0.08f,0.8f,0.87f);
+	glVertex2f(-4.0f, 4.0f); //G
+	glVertex2f(-4.0f,-2.0f); //H
+	glVertex2f(-4.0f,-2.0f); //H
+	glVertex2f(4.0f,-2.0f); //I
+	glVertex2f(4.0f,-2.0f); //I
+	glVertex2f(4.0f,4.0f); //J
+	glVertex2f(4.0f,4.0f); //J
+	glVertex2f(8.0f,4.0f); //K
+	glVertex2f(8.0f,4.0f); //K
+	glVertex2f(8.0f,8.0f); //L
+	glVertex2f(8.0f,8.0f); //L
+	glVertex2f(-8.0f,8.0f); //E
+	glVertex2f(-8.0f,8.0f); //E
+	glVertex2f(-8.0f,4.0f); //F
+	glVertex2f(-8.0f,4.0f); //F
+	glVertex2f(-4.0f, 4.0f); //G
+	glEnd();
 	
-	initRendering();
-	glutDisplayFunc(drawScene);
-	glutReshapeFunc(handleResize);
-	bgc();
-	glutMainLoop();
-	return 0;
+	glBegin(GL_LINES); // r arm
+	glColor3f(0.87f,0.65f,0.29f);
+	glVertex2f(-8.0f,-4.0f);
+	glVertex2f(-4.0f,-4.0f);
+	glVertex2f(-4.0f,-4.0f);
+	glVertex2f(-4.0f,8.0f);
+	glVertex2f(-4.0f,8.0f);
+	glVertex2f(-8.0f,8.0f);
+	glVertex2f(-8.0f,8.0f);
+	glVertex2f(-8.0f,-4.0f);
+	glEnd();
+	
+	glBegin(GL_LINES); // l arm
+	glColor3f(0.87f,0.65f,0.29f);
+	glVertex2f(4.0f,-4.0f);
+	glVertex2f(8.0f,-4.0f);
+	glVertex2f(8.0f,-4.0f);
+	glVertex2f(8.0f,8.0f);
+	glVertex2f(8.0f,8.0f);
+	glVertex2f(4.0f,8.0f);
+	glVertex2f(4.0f,8.0f);
+	glVertex2f(4.0f,-4.0f);
+	glEnd();
+	
+	glBegin(GL_LINES); //mid
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2f(0.0f, -2.0f);
+	glVertex2f(0.0f, -16.0f);
+	glEnd();
+	
+	glBegin(GL_LINES); // r leg
+	glColor3f(0.28f,0.09,0.68f);
+	glVertex2f(-4.0f,-14.0f);
+	glVertex2f(0.0f,-14.0f);
+	glVertex2f(0.0f,-14.0f);
+	glVertex2f(0.0f,-2.0f);
+	glVertex2f(0.0f,-2.0f);
+	glVertex2f(-4.0f,-2.0f);
+	glVertex2f(-4.0f,-2.0f);
+	glVertex2f(-4.0f,-14.0f);
+	glEnd();
+	
+	glBegin(GL_LINES); // l leg
+	glColor3f(0.28f,0.09,0.68f);
+	glVertex2f(0.0f,-14.0f);
+	glVertex2f(4.0f,-14.0f);
+	glVertex2f(4.0f,-14.0f);
+	glVertex2f(4.0f,-2.0f);
+	glVertex2f(4.0f,-2.0f);
+	glVertex2f(0.0f,-2.0f);
+	glVertex2f(0.0f,-2.0f);
+	glVertex2f(0.0f,-14.0f);
+	glEnd();
+	
+	glBegin(GL_LINES); // foot
+	glColor3f(0.7f,0.7f,0.7f);
+	glVertex2f(-4.0f, -16.0f);
+	glVertex2f(4.0f, -16.0f);
+	glVertex2f(4.0f, -16.0f);
+	glVertex2f(4.0f, -14.0f);
+	glVertex2f(4.0f, -14.0f);
+	glVertex2f(-4.0f, -14.0f);
+	glVertex2f(-4.0f, -14.0f);
+	glVertex2f(-4.0f, -16.0f);
+	glEnd();
 }
 
-void drawScene(){
+void coloredSteve(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -29,6 +118,7 @@ void drawScene(){
 	glPointSize(10.0);
 	
 	glBegin(GL_POINTS); //Patches
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glVertex2f(-2.5, 11.5); // r eye
 	glColor3f(0.29f, 0.15f, 0.56f);
 	glVertex2f(-1.5, 11.5);
@@ -103,12 +193,43 @@ void drawScene(){
 	glutSwapBuffers();
 }
 
+int main(int argc, char** argv) {
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(1920, 1080);
+	glutCreateWindow("Steve");
+	
+	initRendering();
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(handleResize);
+	bgc();
+	glutKeyboardFunc(keyboardFunc);
+	glutMainLoop();
+	return 0;
+}
+
+void drawScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Check the current scene and call the appropriate rendering function
+    if (currentScene == 1) {
+        lineSteve();
+    } else if (currentScene == 2) {
+        coloredSteve();
+    }
+
+    glutSwapBuffers();
+}
+
 void initRendering(){
 	glEnable(GL_DEPTH_TEST);
 }
 
 void handleResize(int w, int h){
-	glViewport(0,0,400,400);
+	glViewport(0,0,200,200);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-20.0,20.0,-20.0,20.0,-20.0,20.0); //left right bottom top near far
@@ -146,6 +267,20 @@ void bgc(){
             glEnd();
         }
     }
-
     glutSwapBuffers();
+}
+
+void keyboardFunc(unsigned char key, int x, int y) {
+    // Handle keyboard input to change the current scene
+    switch (key) {
+        case '1':
+            currentScene = 1;
+            break;
+        case '2':
+            currentScene = 2;
+            break;
+        // Add more cases for scene selection if needed
+    }
+
+    glutPostRedisplay(); // Request a redraw
 }
